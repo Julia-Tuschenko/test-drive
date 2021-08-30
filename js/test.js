b7ceb896ba00a836836a318db89efc95cc39ce2f
Julia Tushchenko <juliayulianna@gmail.com>
2021-08-30 16:15:48 +0300

    js

---
+++ js/test.js
@@ -0,0 +1,45 @@
+// // Change code below this line
+// let productName = "Droid";
+// let pricePerItem = 2000;
+// // console.log(productName);
+// // 'Droid'
+// console.log(productName);
+// // console.log(pricePerItem);
+// // 2000
+// console.log(pricePerItem);
+
+
+// // Change code below this line
+// productName = "Repair droid";
+// pricePerItem = 3500;
+
+// const topSpeed =  160;
+// const distance =  617.54;
+// const login =  "mango935";
+// const isOnline =  true;
+// const isAdmin = false;
+
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// Change code below this line
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + 50;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery ( ${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// function sayHi(){
+//     console.log("Hello,this is my first fuction!");
+//     }
+//     sayHi();
+// Change code below this line
+function add(a,b,c) {
+    console.log(`Addition result equals ${a+b+c}`);
+    // Change code above this line
+  }
+  
+  add(15, 27, 10);
+  add(10, 20, 30);
+  add(5, 10, 15);
