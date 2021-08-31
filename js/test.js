4f83bd49cdd0da6e06e2b2b9937e4a0d19f3f7a2
Julia Tushchenko <juliayulianna@gmail.com>
2021-08-31 16:42:12 +0300

    function

--- js/test.js
+++ js/test.js
@@ -35,11 +35,43 @@
 //     }
 //     sayHi();
 // Change code below this line
-function add(a,b,c) {
-    console.log(`Addition result equals ${a+b+c}`);
+// function add(a,b,c) {
+//     console.log(`Addition result equals ${a+b+c}`);
+//     // Change code above this line
+//   }
+  
+//   add(15, 27, 10);
+//   add(10, 20, 30);
+//   add(5, 10, 15);
+
+  // function add(a, b, c) {
+  //   // Change code below this line
+  // return a + b + c
+  
+  
+  //   // Change code above this line
+  // }
+  
+  // add(2, 5, 8); // 15
+  
+  // console.log(add(15, 27, 10));
+  // console.log(add(10, 20, 30));
+  // console.log(add(5, 10, 15));
+
+  function checkStorage(available, ordered) {
+    let message;
+    // Change code below this line
+  if(available > ordered)
+  message = "Order is processed, our manager will contact you.";
+   else 
+   message = "Not enough goods in stock!";
+  
     // Change code above this line
+    return message;
   }
   
-  add(15, 27, 10);
-  add(10, 20, 30);
-  add(5, 10, 15);
+  console.log(checkStorage(100, 50));
+  console.log(checkStorage(100, 130));
+  console.log(checkStorage(200, 20));
+  console.log(checkStorage(200, 150));
+ console.log(checkStorage(150, 180));
