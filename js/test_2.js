38d04e998cc085d5cbc05c09d07f489f617a2c07
Julia Tushchenko <juliayulianna@gmail.com>
2021-09-08 16:17:15 +0300

    завдання 14

--- js/test_2.js
+++ js/test_2.js
@@ -136,15 +136,73 @@
 // состоящую из слов разделённых только пробелами (параметр message) и цену гравировки одного слова (параметр pricePerWord).
 
 // Напиши тело функции, чтобы она возвращала общую стоимость гравировки всех слов в строке.
-function calculateEngravingPrice(message, pricePerWord) {
-    // Change code below this line
- 
-   const pricePerWord = message.split( );
+// function calculateEngravingPrice(message, pricePerWord) {
+//     // Change code below this line
+
+//     const stringToArray = message.split(" ");
+//     const arrayLength = stringToArray.length;
+//     let totalPrice = arrayLength * pricePerWord;
+//     return totalPrice; 
    
+//     // Change code above this line
+//  }
 
- 
+
+// console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+
+//_______________________________________________________________________________________________________________________________
+
+// Метод массива join()
+// Метод массивов join(delimeter) позволяет соединить элементы массива в строку. В строке 
+// элементы будут разделены символом или группой символов указанных в delimeter. То есть это операция обратная методу строк split(delimeter).
+
+// const words = ["JavaScript", "is", "amazing"];
+// console.log(words.join("")); // 'JavaScriptisamazing'
+// console.log(words.join(" ")); // 'JavaScript is amazing'
+// console.log(words.join("*")); // 'JavaScript*is*amazing'
+
+
+// Задание 12
+// Дополни код функции makeStringFromArray(array, delimeter) так, чтобы она возвращала в 
+// переменной string результат соединения элементов массива array c разделителем delimeter - строку.
+
+// function makeStringFromArray(array, delimeter) {
+//     let string;
+//     // Change code below this line
+  
+//   string = array.join(delimeter);
+  
+//     // Change code above this line
+//     return string;
+//   }
+
+//________________________________________________________________________________________________________________________________
+
+// Задание 13
+// Термин slug - это человеко-понятный уникальный идентификатор, который используется 
+// в веб-разработке для создания читабельных URL-адесов.
+
+// Например, вместо того чтобы пользователь увидел в адресной строке mysite.com/posts/1q8fh74tx, 
+// можно сделать slug из названия статьи. В результате адрес получится более приятным для восприятия: mysite.com/posts/arrays-for-begginers.
+
+// Внимание
+// Slug это всегда строка в нижнем регистре, слова которой разделены тире.
+
+// Напиши функцию slugify(title) которая принимает заголовок статьи, параметр title, и 
+// возвращает slug, созданный из этой строки.
+
+// Значением параметра title будут строки, слова которых разделены только пробелами
+// Все символы slug должны быть в нижнем регистре
+// Все слова slug должна быть разделены тире
+
+function slugify(title) {
+    // Change code below this line
+  
+    const slug = title.toLowerCase().split(" ").join("-");
+    return slug;
     // Change code above this line
- }
+  }
 
+  console.log(slugify("Arrays for begginers"));
 
-console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+  
