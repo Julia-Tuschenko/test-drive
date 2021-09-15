f8c83f397073cb03d72c7fb58bc6078a8bcc54b2
Julia-Tuschenko <juliayulianna@gmail.com>
2021-09-15 22:40:05 +0300

    22

--- js/test_3.js
+++ js/test_3.js
@@ -620,7 +620,282 @@
 // Перебери объект apartment используя метод Object.keys() и цикл for...of. Запиши в переменную keys 
 // массив ключей собственных свойств объекта apartment, и добавь в массив values все значения его свойств.
 
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//   };
+//   const values = [];
+//   // Change code below this line
+//   const keys = Object.keys(apartment);
+//   for (const key of keys){
+//    values.push(apartment[key]);
+//   }
+  
+
+//______________________________________________________________________________________________________________
+// Задача. Подсчёт свойств 2.0
+// Задание 14
+// Выполни рефакторинг функции countProps(object) используя метод Object.keys() и, возможно, но необязательно, цикл for...of.
+
+// function countProps(object) {
+//     // Change code below this line
+//     let propCount = 0;
+//         const keys = Object.keys(object);
+//     for (const key of keys) {
+//         propCount += 1;
+//     }
+//     return propCount;
+//     // Change code above this line
+//   }
+
+//______________________________________________________________________________________________________________
+// Метод Object.values()
+// Если метод Object.keys(obj) возвращает массив ключей собственных свойств объекта, то метод Object.values(obj) 
+// возвращает массив значений его собственных свойств. Если в объекте нет свойств, метод Object.values(obj) вернёт пустой массив.
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ["title", "author", "genres", "rating"]
+
+// const values = Object.values(book);
+// console.log(values); // ["The Last Kingdom", "Bernard Cornwell", 8.38]
+// Массив значений свойств также можно перебрать циклом for...of, например для получения общей суммы числовых значений.
+
+// Задание 15
+// Запиши в переменную keys массив ключей собственных свойств объекта apartment, а в переменную values массив всех 
+// значений его свойств. Используй методы Object.keys() и Object.values().
+
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//   };
+//   // Change code below this line
+//   const keys = Object.keys(apartment);
+//   const values = Object.values(apartment);
+  
+//______________________________________________________________________________________________________________
+// Задача. Расходы на зарплату
+// Задание 16
+// Напиши функцию countTotalSalary(salaries) которая принимает объект зарплат, где имя свойства это имя сотрудника, 
+// а значение свойства это зарплата. Функция должна рассчитать общую сумму зарплат сотрудников и вернуть её. 
+// Используй переменную totalSalary для хранения общей суммы зарплаты.
+
+// function countTotalSalary(salaries) {
+//     let totalSalary = 0;
+//     // Change code below this line
+//       const values = Object.values(salaries);
+//     for (const value of values){
+//       totalSalary += value;
+//     }
+//     // Change code above this line
+//     return totalSalary;
+//   }
+
+//   console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+
+//______________________________________________________________________________________________________________
+// Массив объектов
+// В стандартный набор повседневных задач разработчика входит манипуляция массивом однотипных объектов. 
+// Это значит что все объекты в массиве гарантированно будут иметь одинаковый набор свойств, но с разными значениями.
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+// ];
+// Для перебора такого массива используется стандартный цикл for...of. Значения свойств каждого объекта 
+// можно получить используя синтаксис «через точку», так как в каждом объекте набор свойств и их имена 
+// будут одинаковые, отличаются только значения.
+
+// for (const book of books) {
+//   // Объект книги
+//   console.log(book);
+//   // Название
+//   console.log(book.title);
+//   // Автор
+//   console.log(book.author);
+//   // Рейтинг
+//   console.log(book.rating);
+// }
+// Задание 17
+// Перебери массив объектов colors используя цикл for...of. Добавь в массив hexColors значения свойств 
+// hex, а в массив rgbColors значения свойств rgb из всех объектов массива colors.
+
+// const colors = [
+//     { hex: "#f44336", rgb: "244,67,54" },
+//     { hex: "#2196f3", rgb: "33,150,243" },
+//     { hex: "#4caf50", rgb: "76,175,80" },
+//     { hex: "#ffeb3b", rgb: "255,235,59" },
+//   ];
+  
+//   const hexColors = [];
+//   const rgbColors = [];
+//   // Change code below this line
+//   for (const color of colors){
+//     hexColors.push(color.hex);
+//     rgbColors.push(color.rgb);
+//   }
+//______________________________________________________________________________________________________________
+// Задача. Поиск объекта по значению свойства
+// Задание 18
+// Напиши функцию getProductPrice(productName) которая принимает один параметр productName - название продукта. 
+// Функция ищет объект продукта с таким именем (свойство name) в массиве products и возвращает его цену (свойство price). 
+// Если продукт с таким названием не найден, функция должна возвращать null.
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   function getProductPrice(productName) {
+//     // Change code below this line
+//   for (const product of products){
+//     if (productName === product.name){
+//         return product.price;
+//     }
+//   }
+//   return null;
+//     // Change code above this line
+//   }
+//   console.log(getProductPrice("Radar"));
+
+//______________________________________________________________________________________________________________
+// Задача. Коллекция значений свойства
+// Задание 19
+// Напиши функцию getAllPropValues(propName) которая принимает один параметр propName - имя (ключ) свойства. 
+// Функция должна вернуть массив всех значений свойства с таким именем из каждого объекта в массиве products. 
+// Если в объектах нет свойства с таким именем, функция должна вернуть пустой массив.
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   function getAllPropValues(propName) {
+//     // Change code below this line
+    
+//     const prodNames = [];  
+//   for (const product of products){
+//   if(product[propName]){
+//    prodNames.push(product[propName]);
+//   }
+//   }
+//   return prodNames;
+
+//______________________________________________________________________________________________________________
+// Задача. Общая стоимость товара
+// Задание 20
+// Напиши функцию calculateTotalPrice(productName) которая принимает один параметр productName - название товара.
+// Функция должна вернуть общую стоимость (цена * количество) товара с таким именем из массива products.
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   function calculateTotalPrice(productName) {
+//     // Пиши код ниже этой строки
+//   let totalPrice = 0;
+//   for (const product of products){
+//    if (productName === product.name){
+//     totalPrice = product.price * product.quantity;
+//    }
+//   }
+//   return totalPrice;
+//     // Пиши код выше этой строки
+//   }
+
+//______________________________________________________________________________________________________________
+// Деструктуризация объектов
+// Сложные данные всегда представлены объектом. Множественные обращения к свойствам объекта визуально загрязняют код.
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   public: true,
+//   rating: 8.38,
+// };
+
+// const accessType = book.public ? "публичном" : "закрытом";
+// const message = `Книга ${book.title} автора ${book.author} с рейтингом ${book.rating} находится в ${accessType} доступе.`;
+// Деструктуризация позволяет «распаковать» значения свойств объекта в локальные переменные. Это делает код в месте 
+// их использования менее «шумным».
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   public: true,
+//   rating: 8.38,
+// };
+
+// // Деструктуризируем
+// const { title, author, public, rating, coverImage } = book;
+// console.log(coverImage); // undefined
+
+// const accessType = public ? "публичном" : "закрытом";
+// const message = `Книга ${title} автора ${author} с рейтингом ${rating} находится в ${accessType} доступе.`;
+// Деструктуризация всегда находится в левой части операции присвоения. Переменным внутри фигурных скобок присваиваются 
+// значения свойств объекта. Если имя переменной и имя свойства совпадают, то происходит присваивание, в противном случае 
+// ей будет присвоено undefined. Порядок объявления переменных в фигурных скобках не важен.
+
+// Задание 21
+// Пришел трёхдневный прогноз максимальных температур и мы считаем среднюю температуру за три дня (meanTemperature). 
+// Замени объявления переменных yesterday, today и tomorrow одной операцией деструктуризации свойств объекта highTemperatures.
+
+// const highTemperatures = {
+//     yesterday: 28,
+//     today: 26,
+//     tomorrow: 33,
+//   };
+//   // Change code below this line
+  
+//   const {yesterday, today ,tomorrow}  = highTemperatures;
+  
+//   // Change code above this line
+//   const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+//______________________________________________________________________________________________________________
+
+
+//______________________________________________________________________________________________________________
+
+//______________________________________________________________________________________________________________
+
+//______________________________________________________________________________________________________________
+//______________________________________________________________________________________________________________
 
 //______________________________________________________________________________________________________________
 //______________________________________________________________________________________________________________
 //______________________________________________________________________________________________________________
+//______________________________________________________________________________________________________________
+//______________________________________________________________________________________________________________
+//______________________________________________________________________________________________________________
+//______________________________________________________________________________________________________________
+//______________________________________________________________________________________________________________
+
